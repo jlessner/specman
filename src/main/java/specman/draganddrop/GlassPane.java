006f8ea90e58d802a305c8cbda87dc7b4d96f5f1
mKnauer <max.knauer@gmx.de>
2020-12-19 18:48:45 +0100

    Refactoring: GlassPane Added Transparency to Insertmark

--- src/main/java/specman/draganddrop/GlassPane.java
+++ src/main/java/specman/draganddrop/GlassPane.java
@@ -10,22 +10,26 @@ import javax.swing.JPanel;
 public class GlassPane extends JPanel {
 	public JPanel panel;
 	int menueHeight;
-	
-	
+
+
 	public GlassPane(int menueHeight) {
-       setOpaque(false);
+		setOpaque(false);
 		setLayout(null);
-        panel = new JPanel();
-        panel.setBackground(new Color(85,85,85));
-        panel.setBounds(100,22, 120, 12);
-        this.menueHeight = menueHeight;
-        add(panel);
-        this.setVisible(true);
-    }
-	
+		createPanel();
+		this.menueHeight = menueHeight;
+		add(panel);
+		this.setVisible(true);
+	}
+
+	private void createPanel() {
+		panel = new JPanel();
+		panel.setBackground(new Color(85,85,85, 95));
+		panel.setBounds(100,22, 120, 12);
+	}
+
 	public void setInputRecBounds(int x, int y, int width, int height) {
 		y= y-menueHeight;
-		panel.setBounds(x, y, width, height);	
+		panel.setBounds(x, y, width, height);
 
 		Specman.instance().setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
 	}
