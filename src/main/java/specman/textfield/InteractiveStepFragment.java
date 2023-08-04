4224db058e7098cdceafd8605fe8f510a49800d2
Jan Lessner <jan.lessner@sn-invent.de>
2023-08-04 17:21:49 +0200

    Refactoring

--- src/main/java/specman/textfield/InteractiveStepFragment.java
+++ src/main/java/specman/textfield/InteractiveStepFragment.java
@@ -2,9 +2,10 @@ package specman.textfield;
 
 /** Tag Interface für grafische Bestandteile eines Schritts, die Ziel einer
  * Interaktion sind, und über die man den betreffenden Schritt im Diagramm
- * suchen kann. Aktuell sind dies zum einen die Textfelder und zum anderen
- * die {@link SchrittNummerLabel}. Letztere sind relevant, weil der User über diese
- * per Drag&Drop einen Schritt verschieben kann. */
+ * suchen kann. Aktuell sind dies zum einen die {@link TextEditArea}s und
+ * zum anderencdie {@link SchrittNummerLabel}. Letztere sind relevant, weil
+ * der User über diese per Drag&Drop einen Schritt verschieben kann. */
 public interface InteractiveStepFragment {
+  /** Dient nur einer Debug-Ausgabe im Falle eines fehlenden Lookups von Schritten. */
   String getText();
 }
