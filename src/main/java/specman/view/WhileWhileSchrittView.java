a2feba428366194bb9eb02c03fb1b612bea7219a
Mauserich <141329676+Mauserich@users.noreply.github.com>
2023-09-01 14:05:21 +0200

    Fix wrong child stepnumber in WhileWhileSchrittView

--- src/main/java/specman/view/WhileWhileSchrittView.java
+++ src/main/java/specman/view/WhileWhileSchrittView.java
@@ -11,15 +11,15 @@ import specman.model.v001.WhileWhileSchrittModel_V001;
  * Spezielle Anzeige einer While-Schleife mit einem abschließenden unteren Balken.
  * Im Nassi-Shneiderman-Standard steht in dieser Form unten noch einmal die Prüfbedingung
  * drin, aber das lassen wir mal weg. Das sieht komisch aus.
- * 
+ *
  * @author less02
  */
 public class WhileWhileSchrittView extends SchleifenSchrittView {
-	
+
 	public WhileWhileSchrittView(EditorI editor, SchrittSequenzView parent, EditorContentModel_V001 initialerText, SchrittID id, Aenderungsart aenderungsart, boolean withDefaultContent) {
 		super(editor, parent, initialerText, id, aenderungsart, true);
 		if (withDefaultContent) {
-			initWiederholsequenz(einschrittigeInitialsequenz(editor, id, aenderungsart));
+			initWiederholsequenz(einschrittigeInitialsequenz(editor, id.naechsteEbene(), aenderungsart));
 		}
 	}
 
